2765a7563b3b5bcd5c9efc6385fcdff0368adb7c
jeffkaberagithara-guru <jeffkaberagithara@gmail.com>
2026-06-18 17:57:51 +0300

    Modified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import HeroSection from "./components/home/HeroSection";
+import CollectionSection from "./components/home/CollectionSection";
+import OurStorySection from "./components/home/OurStorySection";
+import PersonalServiceSection from "./components/home/PersonalServiceSection";
+import Fragrances from "./pages/Fragrances";
+import OurStory from "./pages/OurStory";
+import ScentGuide from "./pages/ScentGuide";
+import Contact from "./pages/Contact";
 import { COLORS, FONTS } from "./constants/theme";
+
+function Home() {
+  return (
+    <div style={{ backgroundColor: COLORS.onyx }}>
+      <HeroSection />
+      <CollectionSection />
+      <OurStorySection />
+      <PersonalServiceSection />
+    </div>
+  );
+}
 
 export default function App() {
   return (
@@ -17,7 +36,12 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/fragrances" element={<Fragrances />} />
+          <Route path="/our-story" element={<OurStory />} />
+          <Route path="/scent-guide" element={<ScentGuide />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
