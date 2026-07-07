3b008bc09aae22d6e959d255ffd862dbae197255
jeffkaberagithara-guru <jeffkaberagithara@gmail.com>
2026-07-07 10:12:49 +0300

    Created env file

---
+++ src/vite-env.d.ts
@@ -0,0 +1,55 @@
+/// <reference types="vite/client" />
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.sass" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.less" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.webp" {
+  const content: string;
+  export default content;
+}
